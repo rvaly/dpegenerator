9a05bf4588f5ac34d4255ddf0bcf275b5480b5f5
r.valy@la-boite-immo.com <Secure456!>
2021-09-03 12:22:12 +0200

    ADD MINOR EVOL

--- src/DpeGenerator.php
+++ src/DpeGenerator.php
@@ -63,12 +63,16 @@ class DpeGenerator
      */
     private $isoCode;
 
+    private $size = self::PRINT_SIZE_TYPE;
 
     /**
      * constant to define the type
      */
     public const DPE_TYPE = 'dpe';
     public const GES_TYPE = 'ges';
+
+    public const PRINT_SIZE_TYPE = 'print';
+    public const WEB_SIZE_TYPE = 'web';
 
     private const KG_CO2_M2 = 'kgCO2/m².an';
     private const KWH_M2 = 'kWh/m².an';
@@ -87,6 +91,22 @@ class DpeGenerator
         $this->json = json_decode(file_get_contents($fileName));
     }
 #region GETTER/SETTER
+
+    /**
+     * @param $generateImage
+     */
+    public function setImageSize($size): void
+    {
+        $this->size = $size;
+    }
+
+    /**
+     * @return bool
+     */
+    private function getImageSize(): ?string
+    {
+        return $this->size;
+    }
 
     /**
      * @param $generateImage
@@ -227,19 +247,64 @@ class DpeGenerator
     private function generateImgDpe()
     {
         if ($letterDpe = $this->getNewLetterDPEG()) {
-            if ($this->json->dpe->{$letterDpe}) {
-                $image = new Imagick(__DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR . $this->json->dpe->{$letterDpe}->img);
-                $draw = new ImagickDraw();
-                $draw->setFontSize(90);
-                $draw->annotation(90 + (20 * (3 - strlen($this->getDpeVal()))), $this->json->dpe->{$letterDpe}->dpe_val, $this->getDpeVal());
-                $draw->setFontSize(25);
-                $draw->annotation(95, $this->json->dpe->{$letterDpe}->dpe_text, self::KWH_M2);
-                $draw->setFontSize(90);
-                $draw->annotation(296 + (20 * (3 - strlen($this->getGesVal()))), $this->json->dpe->{$letterDpe}->ges_val, $this->getGesVal());
-                $draw->setFontSize(25);
-                $draw->annotation(296, $this->json->dpe->{$letterDpe}->ges_text, self::KG_CO2_M2);
+            $dpeConf = $this->json->dpe->{$letterDpe};
+            $dirSource = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR;
+            $fontValue = 90;
+            $fontText = 25;
+            $x_dpe_val = 90 + (20 * (3 - strlen($this->getDpeVal())));
+            $x_dpe_text = 95;
+            $x_ges_val = 296 + (20 * (3 - strlen($this->getGesVal())));
+            $x_ges_text = 296;
+
+            if ($this->getImageSize() === self::WEB_SIZE_TYPE && isset($this->json->web)) {
+                $dpeConf = $this->json->web->dpe->{$letterDpe};
+                $dirSource = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR;
+                $fontValue = 58;
+                $fontText = 12;
+                $x_dpe_val = 6 + (20 * (3 - strlen($this->getDpeVal())));
+                $x_dpe_text = 25;
+                $x_ges_val = 90 + (20 * (3 - strlen($this->getGesVal())));
+                $x_ges_text = 110;
+            }
+
+            if ($dpeConf) {
+                $image = new Imagick($dirSource . $dpeConf->img);
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
+                $draw->setFontSize($fontValue);
+                $image->annotateimage($draw, $x_dpe_val, $dpeConf->val, 0, $this->getDpeVal());
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
+                $draw->setFontSize($fontText);
+                $image->annotateimage($draw, $x_dpe_text, $dpeConf->text, 0, self::KWH_M2);
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
+                $draw->setFontSize($fontValue);
+                $image->annotateimage($draw, $x_ges_val, $dpeConf->val, 0, $this->getGesVal());
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
+                $draw->setFontSize($fontText);
+
+                $image->annotateimage($draw, $x_ges_text, $dpeConf->text, 0, self::KG_CO2_M2);
                 $image->setImageFormat('png');
-                $image->drawImage($draw);
+                $image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
+
                 if ($this->getGenerateImage() && $this->getPathToWriteImage()) {
                     $imgTemporary = $this->getPathToWriteImage() . ($this->getNameOfPicture() ?: 'dpeg_' . $this->getDpeVal() . '_' . $this->getGesVal()) . '.png';
                     $image->writeImage($imgTemporary);
@@ -266,16 +331,32 @@ class DpeGenerator
     private function generateImgGes()
     {
         if ($letterGes = $this->getNewLetterGES()) {
-            if ($this->json->ges->{$letterGes}) {
-                $image = new Imagick(__DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR . $this->json->ges->{$letterGes}->img);
-                $draw = new ImagickDraw();
+            $gesConf = $this->json->ges->{$letterGes};
+            $dirSource = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR;
+            if ($this->getImageSize() === self::WEB_SIZE_TYPE && isset($this->json->web)) {
+                $gesConf = $this->json->web->ges->{$letterGes};
+                $dirSource = __DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR . 'web' . DIRECTORY_SEPARATOR;
+            }
+            if ($gesConf) {
+                $image = new Imagick($dirSource . $gesConf->img);
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
                 $draw->setFontSize(60);
-                $draw->annotation($this->json->ges->{$letterGes}->ges_val, $this->json->ges->{$letterGes}->x_val, $this->getGesVal());
+                $image->annotateimage($draw, $gesConf->ges_val, $gesConf->x_val, 0, $this->getGesVal());
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
                 $draw->setFontSize(15);
-                $draw->annotation($this->json->ges->{$letterGes}->ges_text, $this->json->ges->{$letterGes}->x_val, self::KG_CO2_M2);
-                $image->drawImage($draw);
+                $image->annotateimage($draw, $gesConf->ges_text, $gesConf->x_val, 0, self::KG_CO2_M2);
                 $image->setImageFormat('png');
-                $image->cropImage(475, 530, 80, 220);
+                $image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
                 if ($this->getGenerateImage() && $this->getPathToWriteImage()) {
                     $imgTemporary = $this->getPathToWriteImage() . ($this->getNameOfPicture() ?: 'ges_' . $this->getGesVal()) . '.png';
                     $image->writeImage($imgTemporary);
@@ -371,21 +452,32 @@ class DpeGenerator
         if ($letterDPEG = $this->getLetterDPEGGP()) {
             if ($this->json->dpe->{$letterDPEG}) {
                 $image = new Imagick(__DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $this->isoCode . DIRECTORY_SEPARATOR . "base_cons_dpeg.png");
-                $draw = new ImagickDraw();
-                $draw->setFontSize(40);
-                $nombreTirets = $this->json->dpe->{$letterDPEG}->nombreTirets;
+
+                $imageEtiquette = new Imagick(__DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . "etiquette_cons_void.png");
+                $image->compositeImage($imageEtiquette, Imagick::COMPOSITE_OVER, $this->json->dpe->{$letterDPEG}->img_x, $this->json->dpe->{$letterDPEG}->img_y);
+
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('white');
                 $draw->setFillColor('white');
-                $draw->annotation($this->json->dpe->{$letterDPEG}->value_x, $this->json->dpe->{$letterDPEG}->value_y, $this->getDpeVal());
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(1);
+                $draw->setFontSize(40);
+                $image->annotateimage($draw, $this->json->dpe->{$letterDPEG}->value_x, $this->json->dpe->{$letterDPEG}->value_y, 0, $this->getDpeVal());
+                $draw = new \ImagickDraw();
+                $draw->setStrokeColor('black');
+                $draw->setFillColor('black');
+                $draw->setFont(__DIR__ . DIRECTORY_SEPARATOR . 'fonts' . DIRECTORY_SEPARATOR . 'arial.ttf');
+                $draw->setStrokeWidth(0);
+                $draw->setFontSize(25);
+                $nombreTirets = $this->json->dpe->{$letterDPEG}->nombreTirets;
                 for ($i = 0; $i < $nombreTirets; ++$i) {
                     $tirets .= '-';
                 }
-                $draw->setFillColor('black');
-                $draw->setFontSize(25);
-                $draw->annotation($this->json->dpe->{$letterDPEG}->tiret_x, $this->json->dpe->{$letterDPEG}->tiret_y, $tirets);
-                $imageEtiquette = new Imagick(__DIR__ . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . "etiquette_cons_void.png");
-                $image->compositeImage($imageEtiquette, Imagick::COMPOSITE_OVER, $this->json->dpe->{$letterDPEG}->img_x, $this->json->dpe->{$letterDPEG}->img_y);
+
+                $image->annotateimage($draw, $this->json->dpe->{$letterDPEG}->tiret_x, $this->json->dpe->{$letterDPEG}->tiret_y, 0, $tirets);
+
                 $image->setImageFormat('png');
-                $image->drawImage($draw);
+                $image->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
                 if ($this->getGenerateImage() && $this->getPathToWriteImage()) {
                     $imgTemporary = $this->getPathToWriteImage() . ($this->getNameOfPicture() ?: 'dpeg_' . $this->getDpeVal() . '_' . $this->getGesVal()) . '.png';
                     $image->writeImage($imgTemporary);
@@ -434,5 +526,5 @@ class DpeGenerator
         return null;
     }
     #endregion
-
 }
+
